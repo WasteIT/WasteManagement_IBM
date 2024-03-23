21d33c6e7d3d86a4fd4c1e4848d56f65c8c83456
MrPrip <oliverprip@icloud.com>
2024-03-23 15:40:01 +0100

    Can check/uncheck fractions on the graph

--- ClientApp/src/components/Graph.js
+++ ClientApp/src/components/Graph.js
@@ -2,101 +2,98 @@ import React, { Component } from 'react';
 import Chart from 'chart.js/auto';
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from 'firebase/database';
-
+  
 export default class Graph extends Component {
   chartRef = React.createRef();
   chart = null;
   
-
   state = {
-    sensorData: [{x: "0", y: 0}]
+    sensorData: {},
+    sensorVisibility: {}
   };
-
+  
   async componentDidMount() {
     await this.initializeFirebase();
-    await this.fetchSensorData("/sensor/11");
+    await this.fetchSensorData("/sensor/3", 'Bin 1');
+    await this.fetchSensorData("/sensor/3", 'Bin 2');
+    await this.fetchSensorData("/sensor/3", 'Bin 3');
     this.buildChart();
+  }
+  
+  componentDidUpdate(prevProps) {
+    if (this.props.sensorVisibility !== prevProps.sensorVisibility) {
+      this.toggleDatasetVisibility();
+    }
   }
 
   async initializeFirebase() {
     const firebaseConfig = {
-      apiKey: "AIzaSyBrUXDmmZzLtLWYNdqwh4jhFbQvR51SP4A",
-      authDomain: "wasteit-193de.firebaseapp.com",
-      databaseURL: "https://wasteit-193de-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "wasteit-193de",
-      storageBucket: "wasteit-193de.appspot.com",
-      messagingSenderId: "956930927762",
-      appId: "1:956930927762:web:b0d69690f1091005f04a4b",
-      measurementId: "G-DJY9G44K2M"
+     
     };
-
+  
     const app = initializeApp(firebaseConfig);
   }
 
-  async fetchSensorData() {
+  async fetchSensorData(path, label) {
     const db = getDatabase();
-
-    const sensor3Ref = ref(db, "/sensor/3");
-
+    const sensorRef = ref(db, path);
     const fetchData = () => {
-      onValue(sensor3Ref, (snapshot) => {
+      onValue(sensorRef, (snapshot) => {
         const data = snapshot.val();
         if (data) {
+          const sensorData = [];
           Object.keys(data).forEach((key) => {
             const row = data[key];
             const time = row.Timestamp;
-            const fillLevel = row.fill_level;
-            
-            this.state.sensorData.push({x: time, y: fillLevel});
+            const fillLevel = row.fill_level*Math.random();
+            sensorData.push({ x: time, y: fillLevel });
           });
+          this.setState(prevState => ({
+            sensorData: {
+              ...prevState.sensorData,
+              [label]: sensorData
+            },
+            sensorVisibility: {
+              ...prevState.sensorVisibility,
+              [label]: true
+            }
+          }));
         } else {
           console.log('No data available in the database.');
         }
       });
     };
-
-    fetchData();
-  }
-
-  buildChart() {
-    const chartRef = this.chartRef.current.getContext('2d');
-
-    this.chart = new Chart(chartRef, {
-      type: 'line',
-      data: {
-        //labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'], // Disse skal ændres ift knapperne.
-        datasets: [ 
-          {
-            label: 'Bin 1',
-            data: this.state.sensorData,
-            borderColor: 'rgba(255, 99, 132, 1)',
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          },
-          {/*{
-            label: 'Bin 2',
-            data: [30, 40, 50, 60, 70, 80, 90], //Bin type 2
-            borderColor: 'rgba(54, 162, 235, 1)',
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          },
-          {
-            label: 'Bin 3',
-            data: [40, 50, 60, 70, 80, 90, 100], //Bin type 3, osv...
-            borderColor: 'rgba(75, 192, 192, 1)',
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          },*/}
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Procent Full',
+  
+      fetchData();
+    }
+  
+    buildChart() {
+      const chartRef = this.chartRef.current.getContext('2d');
+  
+      const datasets = Object.keys(this.state.sensorData).map(label => ({
+        label: label,
+        data: this.state.sensorData[label],
+        borderColor: this.getRandomColor(),
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+        hidden: !this.state.sensorVisibility[label]
+      }));
+  
+      this.chart = new Chart(chartRef, {
+        type: 'line',
+        data: {
+          datasets: datasets
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Procent Full',
+              },
             },
-          },
-          x: {
-            title: {
+            x: {
+             title: {
               display: true,
               text: 'Time',
             },
@@ -105,20 +102,27 @@ export default class Graph extends Component {
       },
     });
   }
-
+  
+  
+  toggleDatasetVisibility() {
+    this.chart.data.datasets.forEach(dataset => {
+      dataset.hidden = !this.props.sensorVisibility[dataset.label];
+    });
+    this.chart.update();
+  }
+  
+  getRandomColor() {
+    return `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 1)`;
+  }
+  
   render() {
     return (
       <div>
-        <div><canvas ref={this.chartRef} /></div>
-        {/*<div>
-          <h2>Sensor Data</h2>
-          {this.state.sensorData ? (
-            <pre>{JSON.stringify(this.state.sensorData, null, 2)}</pre>
-          ) : (
-            <p>Loading...</p>
-          )}
-          </div>*/}
+        <div>
+          <canvas ref={this.chartRef} />
+        </div>
       </div>
     );
   }
 }
+  
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -5,16 +5,57 @@ import Form from 'react-bootstrap/Form';
 export default class Layout extends Component {
   static displayName = Layout.name;
 
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      sensorVisibility: {
+        'Bin 1': true,
+        'Bin 2': true,
+        'Bin 3': true,
+        'Other1': true,
+        'Other2': true
+      }
+    };
+
+  }
+
+  toggleSensorVisibility(label) {
+    this.setState(prevState => ({
+      sensorVisibility: {
+        ...prevState.sensorVisibility,
+        [label]: !prevState.sensorVisibility[label]
+      }
+    }));
+  }
+
+
   render() {
     return (
       <main>
         <div className="information_page">
           <Form className="fraction_filter">
-            <Form.Check type="checkbox" id="fraction_1" label="Rest" checked/>
-            <Form.Check type="checkbox" id="fraction_1" label="Plastic" checked/>
-            <Form.Check type="checkbox" id="fraction_1" label="Cardboard" checked/>
-            <Form.Check type="checkbox" id="fraction_1" label="Other"/>
-            <Form.Check type="checkbox" id="fraction_1" label="Other"/>
+          <Form.Check
+              type="checkbox"
+              id="Bin 1"
+              label="Bin 1"
+              checked={this.state.sensorVisibility['Bin 1']}
+              onChange={() => this.toggleSensorVisibility('Bin 1')}
+            />
+            <Form.Check
+              type="checkbox"
+              id="Bin 2"
+              label="Bin 2"
+              checked={this.state.sensorVisibility['Bin 2']}
+              onChange={() => this.toggleSensorVisibility('Bin 2')}
+            />
+            <Form.Check
+              type="checkbox"
+              id="Bin 3"
+              label="Bin 3"
+              checked={this.state.sensorVisibility['Bin 3']}
+              onChange={() => this.toggleSensorVisibility('Bin 3')}
+            />
           </Form>
           <div className='graph_wrapper_outer'>
             <div className='filter_options_wrapper'>
@@ -24,7 +65,7 @@ export default class Layout extends Component {
               <button type='button' className='btn button filter_graph_button'>Custom filter</button>
             </div>
             <div className='graph_wrapper_inner'>
-              <Graph />
+            <Graph sensorVisibility={this.state.sensorVisibility} />
             </div>
           </div>
         </div>
