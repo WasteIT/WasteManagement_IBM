5c5bd5c028d170457946bac60b00a556278f141c
MrPrip <oliverprip@icloud.com>
2024-03-24 12:38:04 +0100

    Having issues with cors

--- ClientApp/src/components/Agreements.js
+++ ClientApp/src/components/Agreements.js
@@ -1,18 +1,64 @@
-import { Component } from "react";
-import ServiceAgrgeementsDropdown from "./ServiceAgreementsDropdown";
+import { Component, useEffect } from "react";
+import ServiceAgreementsDropdown from "./ServiceAgreementsDropdown";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from 'firebase/database';
 
 class Agreements extends Component {
+
+    state = {
+        serviceAgreements: [],
+      };
+      
+      async componentDidMount() {
+        await this.initializeFirebase();
+        await this.fetchSensorData();
+      }
+
+
+  async initializeFirebase() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBrUXDmmZzLtLWYNdqwh4jhFbQvR51SP4A",
+      authDomain: "wasteit-193de.firebaseapp.com",
+      databaseURL: "https://wasteit-193de-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "wasteit-193de",
+      storageBucket: "wasteit-193de.appspot.com",
+      messagingSenderId: "956930927762",
+      appId: "1:956930927762:web:b0d69690f1091005f04a4b",
+      measurementId: "G-DJY9G44K2M"
+    };
+  
+    const app = initializeApp(firebaseConfig);
+  }
+
+  async fetchSensorData() {
+    const db = getDatabase();
+    const Ref = ref(db, "/");
+    
+    const fetchData = () => {
+        onValue(Ref, (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+                const parentKeys = Object.keys(data);
+                this.setState({ serviceAgreements: parentKeys });
+            }
+        });
+    }
+      fetchData();
+    };
+
     render() {
+
         return (
            <main className="main">
                 <div className="input-group">
                     <input type="search" className="form-control search_bar" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
                     <button type="button" className="btn search_bar_button" data-mdb-ripple-init>search</button>
                 </div>
-
-                <ServiceAgrgeementsDropdown/>
-                <ServiceAgrgeementsDropdown/>
-
+                <div>
+                {this.state.serviceAgreements.map(item => (
+                    <ServiceAgreementsDropdown name={item}/>
+        ))}
+                </div>
            </main>
         );
     }
--- ClientApp/src/components/ServiceAgreementsDropdown.js
+++ ClientApp/src/components/ServiceAgreementsDropdown.js
@@ -3,20 +3,16 @@ import Accordion from 'react-bootstrap/Accordion';
 import { NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-class ServiceAgrgeementsDropdown extends Component {
+class ServiceAgreementsDropdown extends Component {
     render() {
+        const { name } = this.props;
+
         return (
             <Accordion className="dropdown" defaultActiveKey="1">
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>Jægersborggade 23</Accordion.Header>
+                    <Accordion.Header>{name}</Accordion.Header>
                     <Accordion.Body>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p>Content for {name}</p>
                         <NavLink tag={Link} className="btn button" to="/graph">Agreements</NavLink>
                     </Accordion.Body>
                 </Accordion.Item>
@@ -25,4 +21,4 @@ class ServiceAgrgeementsDropdown extends Component {
     }
 }
 
-export default ServiceAgrgeementsDropdown;
+export default ServiceAgreementsDropdown;
