3ff8495864d81f27f572cc631910025361dcafa1
MrPrip <oliverprip@icloud.com>
2024-03-24 15:41:48 +0100

    Agreements gets data from api, however it only works on localhost. meaning you need to run both the backend and frontend on different ports

--- ClientApp/src/components/Agreements.js
+++ ClientApp/src/components/Agreements.js
@@ -2,6 +2,7 @@ import { Component, useEffect } from "react";
 import ServiceAgreementsDropdown from "./ServiceAgreementsDropdown";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from 'firebase/database';
+import { json } from "react-router-dom";
 
 class Agreements extends Component {
 
@@ -9,45 +10,22 @@ class Agreements extends Component {
         serviceAgreements: [],
       };
       
-      async componentDidMount() {
-        await this.initializeFirebase();
-        await this.fetchSensorData();
-      }
-
-
-  async initializeFirebase() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBrUXDmmZzLtLWYNdqwh4jhFbQvR51SP4A",
-      authDomain: "wasteit-193de.firebaseapp.com",
-      databaseURL: "https://wasteit-193de-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "wasteit-193de",
-      storageBucket: "wasteit-193de.appspot.com",
-      messagingSenderId: "956930927762",
-      appId: "1:956930927762:web:b0d69690f1091005f04a4b",
-      measurementId: "G-DJY9G44K2M"
-    };
-  
-    const app = initializeApp(firebaseConfig);
-  }
-
-  async fetchSensorData() {
-    const db = getDatabase();
-    const Ref = ref(db, "/");
     
-    const fetchData = () => {
-        onValue(Ref, (snapshot) => {
-            const data = snapshot.val();
-            if (data) {
-                const parentKeys = Object.keys(data);
-                this.setState({ serviceAgreements: parentKeys });
-            }
-        });
+    componentDidMount() {
+        this.fetchData();
     }
-      fetchData();
+
+    fetchData = async () => {
+        const response = await fetch('http://localhost:5000/agreement');
+        if (!response.ok) {
+            throw new Error('Failed to fetch data');
+        }
+        const jsonData = await response.json();
+        this.setState({ serviceAgreements: jsonData });
+        console.log(this.state.serviceAgreements);
     };
 
     render() {
-
         return (
            <main className="main">
                 <div className="input-group">
@@ -55,9 +33,9 @@ class Agreements extends Component {
                     <button type="button" className="btn search_bar_button" data-mdb-ripple-init>search</button>
                 </div>
                 <div>
-                {this.state.serviceAgreements.map(item => (
-                    <ServiceAgreementsDropdown name={item}/>
-        ))}
+                    {this.state.serviceAgreements.map(item => (
+                        <ServiceAgreementsDropdown name={item}/>
+                    ))}
                 </div>
            </main>
         );
