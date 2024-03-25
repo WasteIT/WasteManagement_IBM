347a0b3780892517068928df19260c98b175c434
MrPrip <oliverprip@icloud.com>
2024-03-25 13:02:42 +0100

    Added a higher order component. for data fetching

--- ClientApp/src/components/Agreements.js
+++ ClientApp/src/components/Agreements.js
@@ -1,42 +1,67 @@
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
 import ServiceAgreementsDropdown from "./ServiceAgreementsDropdown";
 
+const fetchDataBeforeAgreements = (WrappedComponent) => {
+  return (props) => {
+    const [serviceAgreements, setServiceAgreements] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-class Agreements extends Component {
-
-    state = {
-        serviceAgreements: [],
-      };
-      
-    
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData = async () => {
-        const response = await fetch('https://wasteit-backend.azurewebsites.net/agreement');
-        if (!response.ok) {
-            throw new Error('Failed to fetch data');
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            "https://wasteit-backend.azurewebsites.net/agreement"
+          );
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          const jsonData = await response.json();
+          setServiceAgreements(jsonData);
+          setIsLoading(false);
+        } catch (error) {
+          console.error("Error fetching data: ", error);
+          setIsLoading(false);
         }
-        const jsonData = await response.json();
-        this.setState({ serviceAgreements: jsonData });
-    };
+      };
 
-    render() {
-        return (
-           <main className="main">
-                <div className="input-group">
-                    <input type="search" className="form-control search_bar" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                    <button type="button" className="btn search_bar_button" data-mdb-ripple-init>search</button>
-                </div>
-                <div>
-                    {this.state.serviceAgreements.map(item => (
-                        <ServiceAgreementsDropdown name={item} />
-                    ))}
-                </div>
-           </main>
-        );
-    }
-}
+      fetchData();
 
-export default Agreements
+    }, []);
+
+    return <WrappedComponent {...props} serviceAgreements={serviceAgreements} isLoading={isLoading} />;
+  };
+};
+
+const Agreements = ({ serviceAgreements, isLoading }) => {
+  if (isLoading) {
+    return
+  }
+
+  return (
+    <main className="main">
+      <div className="input-group">
+        <input
+          type="search"
+          className="form-control search_bar"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="search-addon"
+        />
+        <button
+          type="button"
+          className="btn search_bar_button"
+          data-mdb-ripple-init
+        >
+          search
+        </button>
+      </div>
+      <div>
+        {serviceAgreements.map((item, index) => (
+          <ServiceAgreementsDropdown key={index} name={item} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default fetchDataBeforeAgreements(Agreements);
--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -3,144 +3,150 @@ import { useLocation } from 'react-router-dom';
 import Chart from 'chart.js/auto';
 import Form from 'react-bootstrap/Form';
 
-const Layout = () => {
-  const { state: { name } = {} } = useLocation();
+const fetchDataBeforeLayout = (WrappedComponent) => {
+  return (props) => {
+    const { state: { name } = {} } = useLocation();
+    const chartRef = useRef();
+    const [isSensorDataVisible, setIsSensorDataVisible] = useState({});
+    const [sensorData, setSensorData] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [graphInstance, setGraphInstance] = useState(null);
 
-  const chartRef = useRef();
-  const [isSensorDataVisible, setIsSensorDataVisible] = useState({});
-  const [sensorData, setSensorData] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const [graphInstance, setGraphInstance] = useState(null);
-
-  useEffect(() => {
-    const fetchSensorData = async () => {
-      try {
-        const response = await fetch("https://wasteit-backend.azurewebsites.net/data/" + name + "/sensor");
-        if (!response.ok) {
-          throw new Error('Failed to fetch sensor data');
+    useEffect(() => {
+      const fetchSensorData = async () => {
+        try {
+          const response = await fetch("https://wasteit-backend.azurewebsites.net/data/" + name + "/sensor");
+          if (!response.ok) {
+            throw new Error('Failed to fetch sensor data');
+          }
+          const childrenData = await response.json();
+    
+          const initialVisibilityState = {};
+          Object.keys(childrenData).forEach((key, index) => {
+            if (index !== 0) initialVisibilityState[key] = true;
+          });
+          setIsSensorDataVisible(initialVisibilityState);
+    
+          Object.keys(initialVisibilityState).forEach(element => {
+            fetchGraphData(`${name}/sensor/${element}`, element);
+          });
+          
+          setIsLoading(false);
+        } catch (error) {
+          console.error('Error fetching sensor data:', error);
         }
-        const childrenData = await response.json();
-  
-        const initialVisibilityState = {};
-        Object.keys(childrenData).forEach((key, index) => {
-          if (index !== 0) initialVisibilityState[key] = true;
+      };
+      fetchSensorData();
+    }, [name]);
+
+
+    const fetchGraphData = async (path, label) => {
+      try {
+        const response = await fetch("https://wasteit-backend.azurewebsites.net/sensor/" + path);
+        const jsonData = await response.json();
+        const sensorData = [];
+        jsonData.forEach(entry => {
+          const parsedEntry = JSON.parse(entry);
+          const timestamp = parsedEntry[1].Timestamp;
+          const fillLevel = parsedEntry[1].fill_level;
+          sensorData.push({ x: timestamp, y: fillLevel });
         });
-        setIsSensorDataVisible(initialVisibilityState);
-  
-        Object.keys(initialVisibilityState).forEach(element => {
-          fetchGraphData(`${name}/sensor/${element}`, element);
-        });
-        
-        setIsLoading(false);
+        setSensorData(prevState => ({
+          ...prevState,
+          [label]: sensorData
+        }));
+        setIsSensorDataVisible(prevState => ({
+          ...prevState,
+          [label]: true
+        }));
       } catch (error) {
-        console.error('Error fetching sensor data:', error);
+        console.error('Error fetching graph data:', error);
       }
     };
-    fetchSensorData();
-  }, [name]);
 
+    
+    useEffect(() => {
+      if (!isLoading && Object.keys(sensorData).length > 0) {
+        buildChart();
+      }
+    }, [isLoading, sensorData, isSensorDataVisible]);
 
-  const fetchGraphData = async (path, label) => {
-    try {
-      const response = await fetch("https://wasteit-backend.azurewebsites.net/sensor/" + path);
-      const jsonData = await response.json();
-      const sensorData = [];
-      jsonData.forEach(entry => {
-        const parsedEntry = JSON.parse(entry);
-        const timestamp = parsedEntry[1].Timestamp;
-        const fillLevel = parsedEntry[1].fill_level;
-        sensorData.push({ x: timestamp, y: fillLevel });
-      });
-      setSensorData(prevState => ({
-        ...prevState,
-        [label]: sensorData
+    const buildChart = () => {
+      if (graphInstance) {
+        graphInstance.destroy();
+      }
+
+      const chartRefCurrent = chartRef.current.getContext('2d');
+
+      const datasets = Object.keys(sensorData).map(label => ({
+        label: label,
+        data: sensorData[label],
+        borderColor: getRandomColor(label),
+        backgroundColor: 'rgba(255, 255, 255, 0)',
+        hidden: !isSensorDataVisible[label]
       }));
-      setIsSensorDataVisible(prevState => ({
-        ...prevState,
-        [label]: true
-      }));
-    } catch (error) {
-      console.error('Error fetching graph data:', error);
-    }
-  };
 
-  
-  useEffect(() => {
-    if (!isLoading && Object.keys(sensorData).length > 0) {
-     
-      buildChart();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoading, sensorData, isSensorDataVisible]);
-
-  const buildChart = () => {
-    if (graphInstance) {
-      graphInstance.destroy();
-    }
-
-    const chartRefCurrent = chartRef.current.getContext('2d');
-
-    const datasets = Object.keys(sensorData).map(label => ({
-      label: label,
-      data: sensorData[label],
-      borderColor: getRandomColor(label),
-      backgroundColor: 'rgba(255, 255, 255, 0)',
-      hidden: !isSensorDataVisible[label]
-    }));
-
-    const newgraphInstance = new Chart(chartRefCurrent, {
-      type: 'line',
-      data: {
-        datasets: datasets
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-              text: 'Procent Full',
+      const newgraphInstance = new Chart(chartRefCurrent, {
+        type: 'line',
+        data: {
+          datasets: datasets
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              title: {
+                display: true,
+                text: 'Procent Full',
+              },
             },
-          },
-          x: {
-            title: {
-              display: true,
-              text: 'Time',
+            x: {
+              title: {
+                display: true,
+                text: 'Time',
+              },
             },
           },
         },
-      },
-    });
+      });
 
-    setGraphInstance(newgraphInstance);
+      setGraphInstance(newgraphInstance);
+    };
+
+    const toggleisSensorDataVisible = label => {
+      setIsSensorDataVisible(prevState => ({
+        ...prevState,
+        [label]: !prevState[label]
+      }));
+    };
+
+    const getRandomColor = (label) => {
+      switch (label) {
+        case '1':
+          return `rgba(139, 69, 19, 1)`;
+        case '2':
+          return `rgba(50, 50, 50, 1)`;
+        case '3':
+          return `rgba(0, 0, 255, 1)`;
+        case '4':
+          return `rgba(0, 128, 0, 1)`;
+        case '5':
+          return `rgba(255, 255, 0, 1)`;
+        default:
+          return `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 1)`;
+      }
+    };
+
+    return <WrappedComponent {...props} isLoading={isLoading} chartRef={chartRef} toggleisSensorDataVisible={toggleisSensorDataVisible} isSensorDataVisible={isSensorDataVisible} />;
   };
+};
 
-  const toggleisSensorDataVisible = label => {
-    setIsSensorDataVisible(prevState => ({
-      ...prevState,
-      [label]: !prevState[label]
-    }));
-  };
-
-  const getRandomColor = (label) => {
-    switch (label) {
-      case '1':
-        return `rgba(139, 69, 19, 1)`;
-      case '2':
-        return `rgba(50, 50, 50, 1)`;
-      case '3':
-        return `rgba(0, 0, 255, 1)`;
-      case '4':
-        return `rgba(0, 128, 0, 1)`;
-      case '5':
-        return `rgba(255, 255, 0, 1)`;
-      default:
-        return `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 1)`;
-    }
-  };
-
+const Layout = ({ isLoading, chartRef, toggleisSensorDataVisible, isSensorDataVisible }) => {
   return (
     <main>
+      {isLoading ? (
+        <div></div>
+      ) : (
       <div className="information_page">
         <Form className="fraction_filter">
           {Object.keys(isSensorDataVisible).map((label, index) => (
@@ -166,8 +172,9 @@ const Layout = () => {
           </div>
         </div>
       </div>
+    )}
     </main>
   );
 };
 
-export default Layout;
+export default fetchDataBeforeLayout(Layout);
