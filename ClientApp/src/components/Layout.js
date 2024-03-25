f360248c9a071cbb626efdeae25945fbe8a41f75
MrPrip <oliverprip@icloud.com>
2024-03-25 19:45:55 +0100

    Added correct colours for all 10 waste fractions

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -131,16 +131,26 @@ const fetchDataBeforeLayout = (WrappedComponent) => {
 
     const getRandomColor = (label) => {
       switch (label) {
-        case '1':
+        case 'General waste':
+          return `rgba(19, 13, 15, 1)`;
+        case 'Food':
+          return `rgba(0, 178, 90, 1)`;
+        case 'Cardboard':
+          return `rgba(190, 160, 102, 1)`;
+        case 'Metal':
+          return `rgba(87, 109, 122, 1)`;
+        case 'Plastic':
+          return `rgba(146, 52, 148, 1)`;
+        case 'Glass':
+          return `rgba(96, 195, 174, 1)`;
+        case 'Paper':
+          return `rgba(0, 132, 194, 1)`;
+        case 'Carton':
+          return `rgba(187, 155, 106, 1)`;
+        case 'Textiles':
           return `rgba(139, 69, 19, 1)`;
-        case '2':
-          return `rgba(50, 50, 50, 1)`;
-        case '3':
-          return `rgba(0, 0, 255, 1)`;
-        case '4':
-          return `rgba(0, 128, 0, 1)`;
-        case '5':
-          return `rgba(255, 255, 0, 1)`;
+        case 'Dangerous':
+            return `rgba(237, 28, 47, 1)`;
         default:
           return `rgba(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 1)`;
       }
