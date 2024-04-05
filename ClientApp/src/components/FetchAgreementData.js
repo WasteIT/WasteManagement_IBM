3519263ed03eb27b7ce25ea77b8529b7fb857cb8
MrPrip <oliverprip@icloud.com>
2024-04-05 09:13:17 +0200

    Added the backend instead of localhost

--- ClientApp/src/components/FetchAgreementData.js
+++ ClientApp/src/components/FetchAgreementData.js
@@ -7,7 +7,7 @@ export default function FetchAgreementData (){
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:5000/agreement");
+        const response = await fetch("https://wasteit-backend.azurewebsites.net/agreement");
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
