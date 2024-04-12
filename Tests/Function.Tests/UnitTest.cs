5a18b9ca75394aa2d00f2e74343dc41e5c8a010b
MadsOrfelt <orfe@itu.dk>
2024-04-12 15:30:40 +0200

    added multiple tests for the WasteBinManager class
    they have all passed

--- Tests/Function.Tests/UnitTest.cs
+++ Tests/Function.Tests/UnitTest.cs
@@ -15,9 +15,6 @@ namespace Function.Tests
             WasteCategory plastic = new WasteCategory("Plastic", 7, new List<int> { 6 });
             WasteCategory paper = new WasteCategory("Paper", 6, new List<int> { 5 });
             WasteCategory glass = new WasteCategory("Glass", 5, new List<int> { 4});
-
-            //WasteBin bin1 = new WasteBin(1, 120, 0.7, plastic, manager);
-            //WasteBin bin2 = new WasteBin(1, 120, 0.7, paper, manager);
             
             manager.addWasteCategory(plastic);
             manager.addWasteCategory(paper);
@@ -30,6 +27,32 @@ namespace Function.Tests
             {
                 Assert.True(wasteCategory.wasteAmount > 0);
             }
+        }
+        [Fact]
+        public void calculateWasteShareForEachBin()
+        {
+            //Arrange
+            WasteBinManager manager = new WasteBinManager();
+            WasteCategory plastic = new WasteCategory("Plastic", 7, new List<int> { 6 });
+
+            WasteBin bin1 = new WasteBin(1, 120, 0.7, plastic, manager);
+            WasteBin bin2 = new WasteBin(2, 120, 1, plastic, manager);
+            WasteBin bin3 = new WasteBin(3, 120, 0.9, plastic, manager);
+
+            manager.addWasteCategory(plastic);
+            plastic.AddWasteBin(bin1);
+            plastic.AddWasteBin(bin2);
+            plastic.AddWasteBin(bin3);
+
+            plastic.wasteAmount = 10;
+            
+            //Act
+            manager.calculateWasteShareForEachBin();
+            
+            
+            //Assert
+            double totalWaste = bin1.share + bin2.share + bin3.share;
+            Assert.Equal(10, totalWaste);
         }
     }
 }
