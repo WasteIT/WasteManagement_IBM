53df816f353b91f829add7af1716988c3491c1ce
Marius Stokkebro <masto@itu.dk>
2024-04-16 15:04:25 +0200

    added a test

---
+++ Tests/PlaywrightTests/UITest1.cs
@@ -0,0 +1,21 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    [Test]
+    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    {
+        await Page.GotoAsync("https://wasteit.azurewebsites.net/");
+
+
+
+    }
+}
