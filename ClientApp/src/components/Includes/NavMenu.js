4e41fbe758f707f4ce6aee8966c0a48574a6f72a
MadsOrfelt <orfe@itu.dk>
2024-04-25 13:12:30 +0200

    more css

--- ClientApp/src/components/Includes/NavMenu.js
+++ ClientApp/src/components/Includes/NavMenu.js
@@ -25,7 +25,7 @@ export function NavMenu() {
               </NavItem>
               
               {true ? (
-                <div className='logged_out_wrapper' style={{display: 'flex'}}>
+                <div className='logged_out_wrapper' style={{display: 'flex', marginLeft: 500}}>
                   <NavItem>
                     <NavLink tag={Link} className="" to="/Login">Login</NavLink>
                   </NavItem>
