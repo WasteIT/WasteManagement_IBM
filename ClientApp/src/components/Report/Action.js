fdd971480eb0db285d62c5b673e82d847a048e9e
cpilITU <cpil@itu.dk>
2024-05-02 10:54:14 +0200

    Tried to make it work again

--- ClientApp/src/components/Report/Action.js
+++ ClientApp/src/components/Report/Action.js
@@ -1,5 +1,3 @@
-import { useEffect } from 'react';
-import Card from 'react-bootstrap/Card';
 export default function Action({fraction, action, binId, numberOfBins}){
 
     function GetText(action) {
@@ -33,10 +31,10 @@ export default function Action({fraction, action, binId, numberOfBins}){
 
     return (
             <div style={{display: 'flex', padding: '0.5rem'}}>
-                <img style={{height: '4rem', borderRadius: '13px'}} src={`./images/${fraction}.png`} alt="Card image cap"/>
+                <img style={{height: '4rem', borderRadius: '13px'}} src={`./images/${fraction}.png`} alt="Card cap"/>
                 
                 <div style={{alignItems: 'center', display: 'flex', background: GetColor(action), borderRadius: '25px',boxShadow: '-10px 10px 20px rgba(33, 82, 75, 0.5)',width: '30rem',marginLeft: '2rem'}}>
-                    <img style={{width: '2rem', marginLeft: '0.5rem', marginRight: '0.5rem'}} src={`./images/${action}.png`} alt="Card image cap"/>
+                    <img style={{width: '2rem', marginLeft: '0.5rem', marginRight: '0.5rem'}} src={`./images/${action}.png`} alt="Card cap"/>
                     <div style= {{marginTop: '1rem'}}>
                         <p>{GetText(action)}</p> 
                     </div>
