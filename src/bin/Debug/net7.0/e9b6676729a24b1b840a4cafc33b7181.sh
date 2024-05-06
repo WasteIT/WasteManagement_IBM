ab6fd8dcc89f41328fcee4d2eb8939288ca7afc6
edward.rostomian@gmail.com <36159443+EdwardR7@users.noreply.github.com>
2024-05-06 14:12:08 +0200

    adjusted shadows agreement and background in graph

---
+++ src/bin/Debug/net7.0/e9b6676729a24b1b840a4cafc33b7181.sh
@@ -0,0 +1,30 @@
+function list_child_processes () {
+    local ppid=$1;
+    local current_children=$(pgrep -P $ppid);
+    local local_child;
+    if [ $? -eq 0 ];
+    then
+        for current_child in $current_children
+        do
+          local_child=$current_child;
+          list_child_processes $local_child;
+          echo $local_child;
+        done;
+    else
+      return 0;
+    fi;
+}
+
+ps 39431;
+while [ $? -eq 0 ];
+do
+  sleep 1;
+  ps 39431 > /dev/null;
+done;
+
+for child in $(list_child_processes 39438);
+do
+  echo killing $child;
+  kill -s KILL $child;
+done;
+rm /Users/edwardrostomian/Documents/GitHub/WasteManagement_IBM/src/bin/Debug/net7.0/e9b6676729a24b1b840a4cafc33b7181.sh;
