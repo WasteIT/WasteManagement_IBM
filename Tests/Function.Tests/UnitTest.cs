71df2091e684c537d75be9796fa768cfa4f677ba
MrPrip <oliverprip@icloud.com>
2024-05-13 20:46:08 +0200

    .

--- Tests/Function.Tests/UnitTest.cs
+++ Tests/Function.Tests/UnitTest.cs
@@ -139,33 +139,7 @@ namespace Function.Tests
                 Assert.True(wasteCategory.wasteAmount > 0);
             }
         }
-        [Fact]
-        public void calculateWasteShareForEachBin()
-        {
-            //Arrange
-            WasteBinManager manager = new WasteBinManager();
-            WasteCategory plastic = new WasteCategory("Plastic", 7, new List<int> { 6 });
-
-            WasteBin bin1 = new WasteBin("1", 120, 0.7, plastic, manager);
-            WasteBin bin2 = new WasteBin("2", 120, 1, plastic, manager);
-            WasteBin bin3 = new WasteBin("3", 120, 0.9, plastic, manager);
-
-            manager.addWasteCategory(plastic);
-            plastic.AddWasteBin(bin1);
-            plastic.AddWasteBin(bin2);
-            plastic.AddWasteBin(bin3);
-
-            plastic.wasteAmount = 10;
-            
-            //Act
-            manager.calculateWasteShareForEachBin();
-            
-            
-            //Assert
-            double totalWastePercent = bin1.share + bin2.share + bin3.share;
-            int roundedTotalWastePercent = (int)Math.Ceiling(totalWastePercent);
-            Assert.Equal(roundedTotalWastePercent, 1);
-        }
+        
         [Fact]
         public void EmptyBinsOnSchedule_EmptiesBinsOnMatchingScheduleDay()  
         {
