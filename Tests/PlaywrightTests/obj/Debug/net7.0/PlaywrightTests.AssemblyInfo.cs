4aeb6343f31c9ad33271c7fa96357b5c7d949420
MrPrip <oliverprip@icloud.com>
2024-05-13 22:50:47 +0200

    .

--- Tests/PlaywrightTests/obj/Debug/net7.0/PlaywrightTests.AssemblyInfo.cs
+++ Tests/PlaywrightTests/obj/Debug/net7.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b0d192eb3ceb5d328ae2460fcf39c644f69a99")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
